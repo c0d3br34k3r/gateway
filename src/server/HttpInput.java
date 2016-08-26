5dcc37c3dbe1af7e43e2d459874f323823c886ee
Max Koren <maxkoren@gmail.com>
2016-08-26 16:20:11 -0400

    made ContentInputStream an inner class

--- src/server/HttpInput.java
+++ src/server/HttpInput.java
@@ -59,11 +59,58 @@ public class HttpInput {
 	}
 
 	public InputStream streamContent(int count) {
-		return new ContentInputStream(in, count);
+		return new ContentInputStream(count);
 	}
 
 	public InputStream streamChunked() throws IOException {
 		return new ChunkedInputStream();
+	}
+
+	public class ContentInputStream extends InputStream {
+
+		private long remaining;
+
+		ContentInputStream(long limit) {
+			this.remaining = limit;
+		}
+
+		@Override public int available() throws IOException {
+			return (int) Math.min(in.available(), remaining);
+		}
+
+		@Override public int read() throws IOException {
+			if (remaining == 0) {
+				return -1;
+			}
+			int result = in.read();
+			if (result != -1) {
+				--remaining;
+			}
+			return result;
+		}
+
+		@Override public int read(byte[] b, int off, int len) throws IOException {
+			if (remaining == 0) {
+				return -1;
+			}
+			int result = in.read(b, off, (int) Math.min(len, remaining));
+			if (result != -1) {
+				remaining -= result;
+			}
+			return result;
+		}
+
+		@Override public long skip(long n) throws IOException {
+			long skipped = in.skip(Math.min(n, remaining));
+			remaining -= skipped;
+			return skipped;
+		}
+
+		@Override public void close() throws IOException {
+			ByteStreams.skipFully(in, remaining);
+			remaining = 0;
+		}
+
 	}
 
 	private class ChunkedInputStream extends InputStream {
